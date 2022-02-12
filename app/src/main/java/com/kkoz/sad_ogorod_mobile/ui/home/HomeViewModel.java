575056b0ab8baa4708901c568c63bd1c75e37343
Konstantin <32872379+Sweettooth24rus@users.noreply.github.com>
2022-02-12 21:00:56 +0700

    Новое начало

--- app/src/main/java/com/kkoz/sad_ogorod_mobile/ui/home/HomeViewModel.java
+++ app/src/main/java/com/kkoz/sad_ogorod_mobile/ui/home/HomeViewModel.java
@@ -1,4 +1,4 @@
-package com.kkoz.sadogorod.ui.home;
+package com.kkoz.sad_ogorod_mobile.ui.home;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
@@ -6,7 +6,7 @@ import androidx.lifecycle.ViewModel;
 
 public class HomeViewModel extends ViewModel {
 
-    private MutableLiveData<String> mText;
+    private final MutableLiveData<String> mText;
 
     public HomeViewModel() {
         mText = new MutableLiveData<>();
--- app/src/test/java/com/kkoz/sad_ogorod_mobile/ExampleUnitTest.java
+++ app/src/test/java/com/kkoz/sad_ogorod_mobile/ExampleUnitTest.java
@@ -1,8 +1,8 @@
-package com.kkoz.sadogorod;
+package com.kkoz.sad_ogorod_mobile;
+
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
-
-import static org.junit.Assert.*;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
