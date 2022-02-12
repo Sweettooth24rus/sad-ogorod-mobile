943b5cf8c4017988b566898c4fb8b52ed393a148
Konstantin <32872379+Sweettooth24rus@users.noreply.github.com>
2022-02-13 00:18:14 +0700

    Красивости

--- app/src/main/java/com/kkoz/sad_ogorod_mobile/MainActivity.java
+++ app/src/main/java/com/kkoz/sad_ogorod_mobile/MainActivity.java
@@ -2,7 +2,6 @@ package com.kkoz.sad_ogorod_mobile;
 
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -28,13 +27,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         setSupportActionBar(binding.appBarMain.toolbar);
-        binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        binding.appBarMain.fab.setOnClickListener(
+                view -> Snackbar.make(view,
+                        "Replace with your own action", Snackbar.LENGTH_LONG
+                )
+                .setAction("Action", null).show());
         DrawerLayout drawer = binding.drawerLayout;
         NavigationView navigationView = binding.navView;
         // Passing each menu ID as a set of Ids because each
@@ -43,8 +40,12 @@ public class MainActivity extends AppCompatActivity {
                 R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
                 .setOpenableLayout(drawer)
                 .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavController navController = Navigation.findNavController(
+                this, R.id.nav_host_fragment_content_main
+        );
+        NavigationUI.setupActionBarWithNavController(
+                this, navController, mAppBarConfiguration
+        );
         NavigationUI.setupWithNavController(navigationView, navController);
     }
 
@@ -57,7 +58,9 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        NavController navController = Navigation.findNavController(
+                this, R.id.nav_host_fragment_content_main
+        );
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
     }
