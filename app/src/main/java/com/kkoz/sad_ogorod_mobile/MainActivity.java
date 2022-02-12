8a5b84615e235df4a52d7b376dcbecf7d362f9ae
Konstantin <32872379+Sweettooth24rus@users.noreply.github.com>
2022-02-13 00:26:43 +0700

    Тестирую ssh

--- app/src/main/java/com/kkoz/sad_ogorod_mobile/MainActivity.java
+++ app/src/main/java/com/kkoz/sad_ogorod_mobile/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(binding.appBarMain.toolbar);
         binding.appBarMain.fab.setOnClickListener(
                 view -> Snackbar.make(view,
-                        "Replace with your own action", Snackbar.LENGTH_LONG
+                        "Replace with your action", Snackbar.LENGTH_LONG
                 )
                 .setAction("Action", null).show());
         DrawerLayout drawer = binding.drawerLayout;
